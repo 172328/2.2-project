eedca666518ce40aa0e709a4edfef6709e9740f5
172328 <172328@cloud.rangitoto.school.nz>
2025-05-27 13:32:28 +1200

    spent today trying to fix and connect github but starting menu function

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
 menu_items={
-1.Kāwakawa Spritzer
-2.Pork and Pūhā Slider
-3.Horopito Fish
-Collars
-4.
-Kāwakawa
-Mussels
-5.
-Taro and Coconut
-Fritters
-6.
-Kūmara and Fennel
-Salad
-Premium
-
-7.
-Pāua and Prawn
-Dumplings
-8.
-Kina
-Canapés
-9.
-Kumara and Truffle
-Ravioli
-10.
-Mānuka
-Smoked
-Salmon
-11.
-Pāua
-Porridge
-
+    Kawakawa Spritzer
+    Pork and Puha Slider
+    Horopito Fish Collars
+    Kawakawa Mussels
+    Taro and Coconut Fritters
+    Kumara and Fennel Salad
+    Paua and Prawn Dumplings
+    Kina Canapes
+    Kumara and Truffle Ravioli
+    Manuka Smoked Salmon
+    "Paua Porridge"
 }
 
 def menu

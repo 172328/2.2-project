4e5ae036b62b63022837a4bedff547054999e3a3
172328 <172328@cloud.rangitoto.school.nz>
2025-05-27 13:24:32 +1200

    Accidentally did not connect github to pycharm for week 1-2
    I planed out the structure of code

---
+++ main.py
@@ -0,0 +1,36 @@
+menu_items={
+1.Kāwakawa Spritzer
+2.Pork and Pūhā Slider
+3.Horopito Fish
+Collars
+4.
+Kāwakawa
+Mussels
+5.
+Taro and Coconut
+Fritters
+6.
+Kūmara and Fennel
+Salad
+Premium
+
+7.
+Pāua and Prawn
+Dumplings
+8.
+Kina
+Canapés
+9.
+Kumara and Truffle
+Ravioli
+10.
+Mānuka
+Smoked
+Salmon
+11.
+Pāua
+Porridge
+
+}
+
+def menu

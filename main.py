6a3915e45b898f92a5eaf7ccf7574a966dd09529
172328 <172328@cloud.rangitoto.school.nz>
2025-05-28 15:14:09 +1200

    Almost finished with menu functions, just need to finish select menu.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 menu_items={
-    Kawakawa Spritzer
-    Pork and Puha Slider
-    Horopito Fish Collars
-    Kawakawa Mussels
-    Taro and Coconut Fritters
-    Kumara and Fennel Salad
-    Paua and Prawn Dumplings
-    Kina Canapes
-    Kumara and Truffle Ravioli
-    Manuka Smoked Salmon
-    "Paua Porridge"
+    1: ("Kawakawa Spritzer", 6),
+    2: ("Pork and Puha Slider", 6),
+    3: ("Horopito Fish Collars", 6),
+    4: ("Kawakawa Mussels", 6),
+    5: ("Taro and Coconut Fritters", 6),
+    6: ("Kumara and Fennel Salad", 6),
+    7: ("Paua and Prawn Dumplings", 9),
+    8: ("Kina Canapes", 9),
+    9:("Kumara and Truffle Ravioli", 9),
+    10:("Manuka Smoked Salmon", 9),
+    11: ("Paua Porridge", 9)
 }
 
-def menu
+def display_menu():
+    print("Rangitoto Restaurant Menu")
+    for number, (name, price) in menu_items.items():
+        print(f"{number}. {name} - ${price}")
+
+def select_menu():
+
+
+display_menu()
